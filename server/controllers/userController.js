2040a9c1bc110a2bd740792b75e51a5d433df94a
Sumit Kumar <developer.sumit.professional@gmail.com>
2025-08-09 16:43:19 +0530

    Authentication System Added

---
+++ server/controllers/userController.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js";
+
+export const getUserData = async (req, res) =>{
+    try {
+        const {userId} = req.body;
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.json({success: false, message: "User not found"});
+        }
+
+        res.json({
+            success: true, 
+            userData:{
+                fullname: user.fullname,
+                isAccountVerified: user.isAccountVerified
+            }
+        })
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
