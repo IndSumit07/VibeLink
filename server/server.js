665d84d76aa303b38ddf584030a90d0500b32520
Sumit Kumar <developer.sumit.professional@gmail.com>
2025-08-11 19:48:49 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', "https://vibelink-ten.vercel.app"]
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
